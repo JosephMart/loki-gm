33872b8557666614ab359b1acf1e897e7036e5c4
Tristan Partin <tristan@partin.io>
2020-04-29 19:14:40 -0500

    Fix lint issue

--- api/ping.ts
+++ api/ping.ts
@@ -10,6 +10,7 @@ import EnvConfigService from "../src/services/EnvConfigService";
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
   const gmService = new GroupMeService(new EnvConfigService());
   await gmService.sendMessage("@Joseph Martinsen hey", [
+    // eslint-disable-next-line @typescript-eslint/camelcase
     { loci: [[0, "@Joseph Martinsen".length]], type: "mentions", user_ids: ["10258869"] },
   ]);
   res.json({ response: "Pong!" });
