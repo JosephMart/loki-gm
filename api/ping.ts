b724a730555c8ad2a95bffa5558acdbfcd3a92c4
Joseph Martinsen <joseph@martinsen.com>
2020-05-08 17:26:38 -0500

    Remove unused imports

--- api/ping.ts
+++ api/ping.ts
@@ -1,7 +1,5 @@
 import "reflect-metadata";
 import { NowRequest, NowResponse } from "@now/node";
-import GroupMeService from "../src/services/GroupMeService";
-import { container } from "tsyringe";
 
 /**
  * Basic health check endpoint to ensure that the lambda is up and
